21a3333f65a78d5bc03ba68f00f2886a8610c6ae
extrude57 <gamemeup8@protonmail.com>
2020-05-15 23:07:35 -0700

    All chips show value to score element but there are bugs

--- src/src2.js
+++ src/src2.js
@@ -13,6 +13,87 @@ let droppableBelow = null;
 
 
 
+  /* 1 dollar chip */
+  
+  function oneclick(e){
+    //var ff=e.value();
+    var f = e.getAttribute("value");
+    console.log(f);
+    console.log("hi");
+  
+  
+  
+  ball.onmousedown = function(event) { // (1) start the process
+    
+    // Can place in moveAt()
+    let shiftX = event.clientX - ball.getBoundingClientRect().left;
+    let shiftY = event.clientY - ball.getBoundingClientRect().top;
+      // (2) prepare to moving: make absolute and on top by z-index
+     
+      ball.style.position = 'absolute';
+      ball.style.zIndex = 1000;
+      // move it out of any current parents directly into body
+      // to make it positioned relative to the body
+      document.body.append(ball);
+      //document.getElementsByClassName('red').style.backgroundColor = "purple";
+      // ...and put that absolutely positioned ball under the pointer
+      // centers the ball at (pageX, pageY) coordinates
+      function moveAt(pageX, pageY) {
+        ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
+        ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+        // Shrink for handle usages
+        ball.style.height = '20px';
+        ball.style.width = '20px';
+  /* TODO: if user presses esc while moving reset it */
+  
+      }
+      moveAt(event.pageX, event.pageY);
+      function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+        ball.hidden = true;
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        ball.hidden = false;
+  
+        if (!elemBelow) return;
+  
+         droppableBelow = elemBelow.closest('.droppable');
+        
+  
+  
+      }
+      
+    
+      
+     
+    
+      // (3) move the ball on mousemove
+      document.addEventListener('mousemove', onMouseMove);
+    
+      // (4) drop the ball, remove unneeded handlers
+      ball.onmouseup = function() {
+        if (currentDroppable != droppableBelow) {
+          if (currentDroppable) { // null when we were not over a droppable before this event
+            leaveDroppable(currentDroppable);
+          }
+          currentDroppable = droppableBelow;
+          if (currentDroppable) { // null if we're not coming over a droppable now
+            // (maybe just left the droppable)
+            enterDroppable(currentDroppable);
+          }
+        }
+        document.removeEventListener('mousemove', onMouseMove);
+        ball.onmouseup = null;
+         //document.getElementsByClassName('.red').style.backgroundColor = "purple";
+      };
+    
+    };
+    ball.ondragstart = function() {
+      return false;
+    };
+    }
+  
+
+
 /* 5 dollar chip */
 
 
@@ -392,8 +473,13 @@ chip5.onmousedown = function(event) { // (1) start the process
   // ToDO: minus the val from the player stack
    // alert('yowie'+val+'yodal');
     elem.style.background = 'purple';
-    
-   // document.getElementById("score").innerHTML = val;
+    console.log(val);
+    document.getElementById('score').innerHTML += val;
+
+
+  
+  
+  
   }
 // 5 dollar
   chip2.ondragstart = function() {
@@ -417,82 +503,6 @@ chip5.onmousedown = function(event) { // (1) start the process
   };
   
 
-  /* 1 dollar chip */
   
-function oneclick(e){
-  //var ff=e.value();
-  var f = e.getAttribute("value");
-  console.log(f);
-  console.log("hi");
-
-
-
-ball.onmousedown = function(event) { // (1) start the process
+    //document.getElementById("div.parent > div#numCon > div.playerArea.player-row > div.child.child-3 > div.score").innerHTML = val;
   
-  // Can place in moveAt()
-  let shiftX = event.clientX - ball.getBoundingClientRect().left;
-  let shiftY = event.clientY - ball.getBoundingClientRect().top;
-    // (2) prepare to moving: make absolute and on top by z-index
-   
-    ball.style.position = 'absolute';
-    ball.style.zIndex = 1000;
-    // move it out of any current parents directly into body
-    // to make it positioned relative to the body
-    document.body.append(ball);
-    //document.getElementsByClassName('red').style.backgroundColor = "purple";
-    // ...and put that absolutely positioned ball under the pointer
-    // centers the ball at (pageX, pageY) coordinates
-    function moveAt(pageX, pageY) {
-      ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
-      ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
-      // Shrink for handle usages
-      ball.style.height = '20px';
-      ball.style.width = '20px';
-/* TODO: if user presses esc while moving reset it */
-
-    }
-    moveAt(event.pageX, event.pageY);
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
-      ball.hidden = true;
-      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-      ball.hidden = false;
-
-      if (!elemBelow) return;
-
-       droppableBelow = elemBelow.closest('.droppable');
-      
-
-
-    }
-    
-  
-    
-   
-  
-    // (3) move the ball on mousemove
-    document.addEventListener('mousemove', onMouseMove);
-  
-    // (4) drop the ball, remove unneeded handlers
-    ball.onmouseup = function() {
-      if (currentDroppable != droppableBelow) {
-        if (currentDroppable) { // null when we were not over a droppable before this event
-          leaveDroppable(currentDroppable);
-        }
-        currentDroppable = droppableBelow;
-        if (currentDroppable) { // null if we're not coming over a droppable now
-          // (maybe just left the droppable)
-          enterDroppable(currentDroppable);
-        }
-      }
-      document.removeEventListener('mousemove', onMouseMove);
-      ball.onmouseup = null;
-       //document.getElementsByClassName('.red').style.backgroundColor = "purple";
-    };
-  
-  };
-  ball.ondragstart = function() {
-    return false;
-  };
-  }
-
